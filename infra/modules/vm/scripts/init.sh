1f74a3945f66e3979916eef96b026953ece34f60
wguzik <wojciech.guzik@gmail.com>
2025-12-01 21:10:22 +0100

    Update Docker Compose installation to version 1.40.3

--- infra/modules/vm/scripts/init.sh
+++ infra/modules/vm/scripts/init.sh
@@ -14,6 +14,8 @@ sh get-docker.sh
 # Add current user to docker group
 usermod -aG docker $USER
 
+newgrp docker
+
 # Install Docker Compose
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.40.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose 
