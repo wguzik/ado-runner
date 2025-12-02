1104e9740dac44705a2df96f8725e60c0c8fae8a
wguzik <wojciech.guzik@gmail.com>
2025-12-02 20:18:53 +0100

    Remove usermod command for docker group
    Removed usermod command to add current user to docker group.

--- infra/modules/vm/scripts/init.sh
+++ infra/modules/vm/scripts/init.sh
@@ -11,9 +11,6 @@ apt-get install -y \
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
-# Add current user to docker group
-usermod -aG docker $USER
-
 # Install Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/2.40.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose 
