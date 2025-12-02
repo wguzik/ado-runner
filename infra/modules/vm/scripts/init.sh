2586248b75aa7d9a01da960370ca6362f31213e7
wguzik <wojciech.guzik@gmail.com>
2025-12-02 20:15:03 +0100

    Remove agent dependencies installation from init.sh
    Removed installation steps for agent dependencies from init.sh.

--- infra/modules/vm/scripts/init.sh
+++ infra/modules/vm/scripts/init.sh
@@ -17,11 +17,3 @@ usermod -aG docker $USER
 # Install Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/2.40.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose 
-
-# Install agent dependencies
-mkdir myagent && cd myagent
-wget https://download.agent.dev.azure.com/agent/4.264.2/vsts-agent-linux-x64-4.264.2.tar.gz
-tar zxvf vsts-agent-linux-x64-4.264.2.tar.gz
-./bin/installdependencies.sh
-
-newgrp docker
